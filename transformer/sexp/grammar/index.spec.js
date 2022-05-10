34a4f59eeaec647c75cc68d0680297bab1b96fdb
Ronald Chen <ronald@battlefy.com>
2022-05-09 21:03:40 -0700

    removed used less sexp grammar test re-worded reference unit test

--- transformer/sexp/grammar/index.spec.js
+++ transformer/sexp/grammar/index.spec.js
@@ -1,11 +1,13 @@
 import { sexp, value, reference } from './index.js'
 
-describe('matcher', () => {
-  describe('reference', () => {
-    test('reference passes through expected', () => {
+describe('grammar', () => {
+  describe('reference to a matcher to allow for cirular depedencies', () => {
+    test('reference can be used and later have the real matcher set on as the field', () => {
       const valueReference = reference()
       const matcher = sexp(valueReference)
+
       valueReference.matcher = value('module')
+
       const result = matcher({
         type: 'sexp',
         value: [{ type: 'value', value: 'module' }],
@@ -13,9 +15,10 @@ describe('matcher', () => {
       expect(result.matched).toBe(true)
     })
 
-    test('throws attempting to be used before value is set', () => {
+    test('throws when matcher has not been set yet', () => {
       const valueReference = reference()
       const matcher = sexp(valueReference)
+
       expect(() =>
         matcher({
           type: 'sexp',
